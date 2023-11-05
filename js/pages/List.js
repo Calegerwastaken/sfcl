d53a7a1493d64d6c93bf0c44d266b2ecd27ec953
Tueml <149439628+Tuemll@users.noreply.github.com>
2023-11-04 20:40:36 -0700

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -26,7 +26,6 @@ export default {
                     <tr v-for="([level, err], i) in list">
                         <td class="rank">
                             <p v-if="i + 1 <= 50" class="type-label-lg">#{{ i + 1 }}</p>
-                            <p v-if="i + 1 = 3" class="type-label-lg">Pinewood</p>
                             <p v-else class="type-label-lg">Legacy</p>
                         </td>
                         <td class="level" :class="{ 'active': selected == i, 'error': !level }">
