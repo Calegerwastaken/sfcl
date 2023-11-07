340ed418250d640cb2c766f20dca6a72305b3051
Tueml <149439628+Tuemll@users.noreply.github.com>
2023-11-07 14:39:40 -0800

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -28,7 +28,8 @@ export default {
                     <table class="board">
                         <tr v-for="(ientry, i) in leaderboard">
                             <td class="rank">
-                                <p class="type-label-lg">#{{ i + 1 }}</p>
+                                <p v-if="i + 1 == 1" class="type-label-lg">#Caleger</p>
+                                <p v-else class="type-label-lg">#{{ i + 1 }}</p>
                             </td>
                             <td class="total">
                                 <p class="type-label-lg">{{ localize(ientry.total) }}</p>
