8c4fab9b13cfa80cb7d074ae2bbcc4a572f99505
Tueml <149439628+Tuemll@users.noreply.github.com>
2023-11-07 14:37:43 -0800

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -120,7 +120,7 @@ export default {
                         Do not use easy modes, only a record of the unmodified level qualifies
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Once a level falls into Pinewood, we no longer accept records
                     </p>
                      <p>
                     </p>
