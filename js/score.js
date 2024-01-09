338ddffa3c0de7ca17dcdebd6dce471b2a661ed1
Tueml <149439628+Tuemll@users.noreply.github.com>
2024-01-08 20:18:58 -0800

    Update score.js

--- js/score.js
+++ js/score.js
@@ -15,7 +15,7 @@ export function score(rank) {
         return 0;
     }
 
-    let score = (250 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50);
+    let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50);
 
     score = Math.max(0, score);
 
