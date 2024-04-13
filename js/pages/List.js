84ba17702d1c7ebba8ff1b225d79fc3427b37ad3
Tueml <149439628+Tuemll@users.noreply.github.com>
2024-04-13 16:16:37 -0700

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -52,7 +52,7 @@ export default {
                         </li>
                         <li>
                             <div class="type-title-sm">Version</div>
-                            <p v-if="level.id > 97000000">#{{ 2.2 }}</p>
+                            <p v-if="level.id > 97000000">{{ 2.2 }}</p>
                             <p v-else>{{ level.version }}</p>
                         </li>
                     </ul>
