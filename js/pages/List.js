67787b6063204c3fca59ffff0e9675479d28924b
Tueml <149439628+Tuemll@users.noreply.github.com>
2024-04-13 16:08:09 -0700

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -50,6 +50,10 @@ export default {
                             <div class="type-title-sm">ID</div>
                             <p>{{ level.id }}</p>
                         </li>
+                        <li>
+                            <div class="type-title-sm">Version</div>
+                            <p>{{ level.version }}</p>
+                        </li>
                     </ul>
                     <h2>Records</h2>
                     <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
